055e98c8820804d37f96ef5938387fe94c7b5294
hellomaddieUwU <info@hellomaddie.com>
2026-04-24 11:17:38 +0800

    debloated index.css page

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './index.css'
+import './main.css'
+import './layout.css'
+import './components.css'
+import './animation.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
